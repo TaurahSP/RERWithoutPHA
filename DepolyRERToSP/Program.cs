a72b33949801e7548170beedaa053a841fefb153
danbudimir <$teelers2>
2015-01-12 10:34:38 -0500

    Local Commit

---
+++ DepolyRERToSP/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+namespace DepolyRERToSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext cc =  new ClientContext("http://win-ija7pajsek8:38370");
+
+            RegisterReceiver(cc, EventReceiverType.ItemAdding, EventReceiverSynchronization.Synchronous);
+            RegisterReceiver(cc, EventReceiverType.ItemUpdating, EventReceiverSynchronization.Synchronous);
+            RegisterReceiver(cc, EventReceiverType.ItemDeleting, EventReceiverSynchronization.Synchronous);
+        }
+
+        private static void RegisterReceiver(ClientContext clientContext, EventReceiverType eventReceiverType, EventReceiverSynchronization synchronization)
+        {
+            List targetList = clientContext.Web.Lists.GetByTitle("Test");
+            clientContext.Load(targetList);
+           
+            EventReceiverDefinitionCollection ec = targetList.EventReceivers;
+            clientContext.Load(ec);
+            clientContext.ExecuteQuery();
+            for (int i = 0; i < ec.Count; i++)
+            {
+                if (ec[i].ReceiverName == "RERSynchronous" && !ec[i].ServerObjectIsNull.Value && ec[i].EventType == eventReceiverType)
+                {
+
+                    try
+                    {
+                        ec[i].DeleteObject();
+                        clientContext.ExecuteQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //log the error but keep going
+                    }
+                }
+            }
+            clientContext.ExecuteQuery();
+
+            EventReceiverDefinitionCreationInformation eventReceiver = new EventReceiverDefinitionCreationInformation();
+            eventReceiver.EventType = eventReceiverType;
+            eventReceiver.ReceiverAssembly = "RERWithoutApp";
+            eventReceiver.ReceiverClass = "RERWithoutApp.Services.RERSynchronous";
+            eventReceiver.ReceiverName = "RERSynchronous";
+            eventReceiver.ReceiverUrl = "http://localhost:19393/Services/RERSynchronous.svc";
+            eventReceiver.SequenceNumber = 1000;
+            eventReceiver.Synchronization = synchronization;
+            targetList.EventReceivers.Add(eventReceiver);
+            clientContext.Web.Context.ExecuteQuery();
+        }
+    }
+}
---
+++ RERWithoutApp/Services/RERSynchronous.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+namespace RERWithoutApp
+{
+    public class RERSynchronous : IRemoteEventService
+    {
+        #region IRemoteEventService implements
+        public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+        {
+            SPRemoteEventResult result = new SPRemoteEventResult();
+            if (properties.ItemEventProperties.AfterProperties["TestChoice"].ToString() == "C1")
+            {
+                result.ChangedItemProperties.Add("Test1", "Changed because TestChoice was c1");
+            }
+            else if (properties.ItemEventProperties.AfterProperties["TestChoice"].ToString() == "C2")
+            {
+                result.ErrorMessage = "No can do because TestChoice was C2";
+                result.Status = SPRemoteEventServiceStatus.CancelWithError;
+            }
+            return result;
+        }
+
+        public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+        {
+            //this event will not fire but must be here to satisfy IRemoteEventService
+        }
+        #endregion
+    }
+}
