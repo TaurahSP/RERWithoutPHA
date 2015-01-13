cb100ace6439d864e7fb910f647c14f66e295a00
danbudimir <$teelers2>
2015-01-12 20:09:08 -0500

    fsdfsd

--- DepolyRERToSP/Program.cs
+++ DepolyRERToSP/Program.cs
@@ -11,11 +11,15 @@ namespace DepolyRERToSP
     {
         static void Main(string[] args)
         {
+            RemoveAll();
+            RegisterAll();
+            
             ClientContext cc =  new ClientContext("http://win-ija7pajsek8:38370");
-
             RegisterReceiver(cc, EventReceiverType.ItemAdding, EventReceiverSynchronization.Synchronous);
             RegisterReceiver(cc, EventReceiverType.ItemUpdating, EventReceiverSynchronization.Synchronous);
             RegisterReceiver(cc, EventReceiverType.ItemDeleting, EventReceiverSynchronization.Synchronous);
+
+
         }
 
         private static void RegisterReceiver(ClientContext clientContext, EventReceiverType eventReceiverType, EventReceiverSynchronization synchronization)
@@ -26,6 +30,7 @@ namespace DepolyRERToSP
             EventReceiverDefinitionCollection ec = targetList.EventReceivers;
             clientContext.Load(ec);
             clientContext.ExecuteQuery();
+            
             for (int i = 0; i < ec.Count; i++)
             {
                 if (ec[i].ReceiverName == "RERSynchronous" && !ec[i].ServerObjectIsNull.Value && ec[i].EventType == eventReceiverType)
@@ -34,7 +39,6 @@ namespace DepolyRERToSP
                     try
                     {
                         ec[i].DeleteObject();
-                        clientContext.ExecuteQuery();
 
                     }
                     catch (Exception ex)
@@ -47,14 +51,85 @@ namespace DepolyRERToSP
 
             EventReceiverDefinitionCreationInformation eventReceiver = new EventReceiverDefinitionCreationInformation();
             eventReceiver.EventType = eventReceiverType;
-            eventReceiver.ReceiverAssembly = "RERWithoutApp";
-            eventReceiver.ReceiverClass = "RERWithoutApp.Services.RERSynchronous";
+            //eventReceiver.ReceiverAssembly = "RERWithoutApp";
+            //eventReceiver.ReceiverClass = "RERWithoutApp.Services.RERSynchronous";
             eventReceiver.ReceiverName = "RERSynchronous";
             eventReceiver.ReceiverUrl = "http://localhost:19393/Services/RERSynchronous.svc";
             eventReceiver.SequenceNumber = 1000;
             eventReceiver.Synchronization = synchronization;
             targetList.EventReceivers.Add(eventReceiver);
             clientContext.Web.Context.ExecuteQuery();
+        }
+        private static void RegisterAll()
+        {
+            string sharePointUrl = "http://win-ija7pajsek8:38370";
+            string remoteWebUrl = "http://localhost:19393";
+            string listName = "Test";
+
+            ClientContext clientContext = new ClientContext(sharePointUrl);
+            List targetList = clientContext.Web.Lists.GetByTitle(listName);
+            clientContext.Load(targetList);
+            EventReceiverDefinitionCollection ec = targetList.EventReceivers;
+            clientContext.Load(ec);
+
+            EventReceiverDefinitionCreationInformation eventReceiver = new EventReceiverDefinitionCreationInformation();
+            eventReceiver.EventType = EventReceiverType.ItemUpdating;
+            //The ReceiverAssembly and ReceiverClass properties, as far as I can tell, are not needed
+            // the RER gets deployed and functions properly with or without them
+            //eventReceiver.ReceiverAssembly = "RERWithoutApp";
+            //eventReceiver.ReceiverClass = "RERWithoutApp.Services.RERSynchronous";
+            eventReceiver.ReceiverName = "RERSynchronous";
+            eventReceiver.ReceiverUrl = remoteWebUrl + "/Services/RERSynchronous.svc";
+            eventReceiver.SequenceNumber = 1000;
+            eventReceiver.Synchronization = EventReceiverSynchronization.Synchronous;
+            targetList.EventReceivers.Add(eventReceiver);
+
+            eventReceiver = new EventReceiverDefinitionCreationInformation();
+            eventReceiver.EventType = EventReceiverType.ItemAdding;
+            eventReceiver.ReceiverName = "RERSynchronous";
+            eventReceiver.ReceiverUrl = remoteWebUrl + "/Services/RERSynchronous.svc";
+            eventReceiver.SequenceNumber = 1000;
+            eventReceiver.Synchronization = EventReceiverSynchronization.Synchronous;
+            targetList.EventReceivers.Add(eventReceiver);
+
+            eventReceiver = new EventReceiverDefinitionCreationInformation();
+            eventReceiver.EventType = EventReceiverType.ItemDeleting;
+            eventReceiver.ReceiverName = "RERSynchronous";
+            eventReceiver.ReceiverUrl = remoteWebUrl + "/Services/RERSynchronous.svc";
+            eventReceiver.SequenceNumber = 1000;
+            eventReceiver.Synchronization = EventReceiverSynchronization.Synchronous;
+            targetList.EventReceivers.Add(eventReceiver);
+            
+            clientContext.Web.Context.ExecuteQuery();
+        }
+        private static void RemoveAll()
+        {
+            string sharePointUrl = "http://win-ija7pajsek8:38370";
+            string listName = "Test";
+
+            ClientContext clientContext = new ClientContext(sharePointUrl);
+            List targetList = clientContext.Web.Lists.GetByTitle(listName);
+            clientContext.Load(targetList);
+            EventReceiverDefinitionCollection ec = targetList.EventReceivers;
+            clientContext.Load(ec);
+            clientContext.ExecuteQuery();
+
+            bool found = true;
+            while (found)
+            {
+                found = false;
+                for (int i = 0; i < ec.Count; i++)
+                {
+                    if (ec[i].ReceiverName == "RERSynchronous" && !ec[i].ServerObjectIsNull.Value)
+                    {
+
+                        ec[i].DeleteObject();
+                        clientContext.ExecuteQuery();
+                        found = true;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
